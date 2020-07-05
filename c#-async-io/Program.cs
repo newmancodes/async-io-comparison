cfd4f12cdb5062a03f218c51b14c131b3bcd0247
Stephen Newman <steve@newman.digital>
2020-07-05 07:21:24 +0100

    Initial commit

---
+++ c#-async-io/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace c__async_io
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Text.Json;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var now = Stopwatch.StartNew();
+            ICollection<Task<JsonDocument>> gets = new List<Task<JsonDocument>>();
+            for (var id = 1; id <= 100; id++)
+            {
+                var get = GetTodo(client, id);
+                gets.Add(get);
+            }
+            await Task.WhenAll(gets);
+            now.Stop();
+            Console.WriteLine($"Elapsed: {now.Elapsed.TotalSeconds} seconds");
+            Console.WriteLine($"Result: {(await gets.Last()).RootElement.ToString()}");
+        }
+
+        private static async Task<JsonDocument> GetTodo(HttpClient client, int id)
+        {
+            const string Base = "https://jsonplaceholder.typicode.com/todos";
+            var address = $"{Base}/{id}";
+            var response = await client.GetAsync(address);
+            var contentStream = await response.Content.ReadAsStreamAsync();
+            return await JsonDocument.ParseAsync(contentStream);
+        }
+    }
+}
---
+++ rust-async-io/src/main.rs
@@ -0,0 +1,26 @@
+use futures::future::try_join_all;
+use reqwest::Client;
+use serde_json::Value;
+use std::{error::Error, time::Instant};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let client = Client::builder().build()?;
+    let now = Instant::now();
+    let mut gets = Vec::new();
+    for id in 1..=100 {
+        let get = get_todo(&client, id);
+        gets.push(get);
+    }
+    let results = try_join_all(gets).await?;
+    println!("Elapsed: {} seconds", now.elapsed().as_secs_f64());
+    println!("Result: {}", results.last().unwrap().to_string());
+    Ok(())
+}
+
+async fn get_todo(client: &Client, id: i32) -> Result<Value, Box<dyn Error>> {
+    let base = "https://jsonplaceholder.typicode.com/todos";
+    let address = format!("{}/{}", base, id);
+    let result = client.get(&address).send().await?.json().await?;
+    Ok(result)
+}
